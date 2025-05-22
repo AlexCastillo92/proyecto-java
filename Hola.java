b814798bb9a95ff108c4639b768233452506c164
AlexCastillo92 <a440299336@my.uvm.edu.mx>
2025-05-21 18:13:37 -0600

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
 public class Hola {
   public static void main(String[] args) {
-    System.out.println("LA VERSION NUEVA MEJORADA 4.0");
+    System.out.println("LA VERSION MEJORADA VERSION FINAL");
   }
 
 }
